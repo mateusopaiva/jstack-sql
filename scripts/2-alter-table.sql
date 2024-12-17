c9afdfd00fb31b66b4410a0da62df8871a9757bb
Mateus Paiva <mateusopaiva@gmail.com>
2024-12-17 11:49:06 -0300

    Alterando as colunas de uma tabela que já existe

--- scripts/2-alter-table.sql
+++ scripts/2-alter-table.sql
@@ -1,3 +1,10 @@
 \c live023
 
-ALTER TABLE customers ADD COLUMN email VARCHAR(256);
+-- ALTER TABLE customers ADD COLUMN email VARCHAR(256);
+-- ALTER TABLE customers ALTER COLUMN first_name SET DATA TYPE VARCHAR(60);
+-- ALTER TABLE customers ADD COLUMN age INT;
+-- ALTER TABLE customers RENAME COLUMN age to bithdate;
+-- ALTER TABLE customers RENAME COLUMN bithdate to age;
+-- ALTER TABLE customers ALTER COLUMN age SET DEFAULT 0;
+-- ALTER TABLE customers DROP COLUMN age;
+ALTER TABLE customers ADD COLUMN age INT DEFAULT 0;
