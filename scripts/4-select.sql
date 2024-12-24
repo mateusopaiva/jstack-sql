bc5bab6e7da9026f5a973dc8764758e36883deef
Mateus Paiva <mateusopaiva@gmail.com>
2024-12-24 17:37:29 -0300

    Trabalhando com o ORDER BY, LIMIT e OFFSET

--- scripts/4-select.sql
+++ scripts/4-select.sql
@@ -1,8 +1,8 @@
 \c live023
 
--- * = ALL
---SELECT id, first_name AS "firstName", last_name AS "lastName"
---FROM customers;
-
 SELECT *
-FROM orders;
+FROM customers
+ORDER BY id ASC
+OFFSET 10
+LIMIT 10
+;
