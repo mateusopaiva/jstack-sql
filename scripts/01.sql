5f212bc79814fe4362f2b2599ddbfefc98f12779
Mateus Paiva <mateusopaiva@gmail.com>
2024-12-31 11:48:12 -0300

    Trabalhando com Aggregate Functions: MAX, MIN, AVG, ROUND e GROUP BY

--- scripts/01.sql
+++ scripts/01.sql
@@ -5,9 +5,18 @@
 -- SELECT * FROM customers ORDER BY id DESC LIMIT 2;
 -- SELECT COUNT(DISTINCT email) AS total_of_customers FROM customers;
 -- INSERT INTO orders(customer_id, amount) VALUES (1, 1000);
+-- SELECT amount FROM orders ORDER BY amount DESC LIMIT 1;
+-- SELECT amount FROM orders ORDER BY amount ASC LIMIT 1;
 
-SELECT 
-  SUM(amount) AS "totalOrders",
-  COUNT(id) AS "totalSales"
-  FROM orders
+-- Funções de Agregação (Aggregate Functions)
+SELECT
+customer_id,
+  COUNT(id) AS total_orders,
+  SUM(amount) AS total_revenue,
+  MAX(amount) AS max_order,
+  MIN(amount) AS min_order,
+  ROUND(AVG(amount), 2) AS average_ticket
+FROM orders
+GROUP BY customer_id
+ORDER BY customer_id
 ;
