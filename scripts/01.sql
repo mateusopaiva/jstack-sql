583122587be34adb2f535d1f702e2104c52ecf2b
Mateus Paiva <mateusopaiva@gmail.com>
2024-12-31 10:48:39 -0300

    Somando valores de colunas com o SUM

--- scripts/01.sql
+++ scripts/01.sql
@@ -1,7 +1,13 @@
 \c live024
 
 -- INSERT INTO customers(first_name) VALUES ('Mateus');
+-- UPDATE customers SET email = 'customer.20@email.com' WHERE id = 21;
+-- SELECT * FROM customers ORDER BY id DESC LIMIT 2;
+-- SELECT COUNT(DISTINCT email) AS total_of_customers FROM customers;
+-- INSERT INTO orders(customer_id, amount) VALUES (1, 1000);
 
-SELECT COUNT(id) AS total_of_customers
-FROM customers
+SELECT 
+  SUM(amount) AS "totalOrders",
+  COUNT(id) AS "totalSales"
+  FROM orders
 ;
