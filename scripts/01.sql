c9a723a826caf4e74782fe037fc2a2c52812e96f
Mateus Paiva <mateusopaiva@gmail.com>
2024-12-31 12:09:02 -0300

    Aplicando filtros em queries com Aggregate Functions com HAVING

--- scripts/01.sql
+++ scripts/01.sql
@@ -7,16 +7,20 @@
 -- INSERT INTO orders(customer_id, amount) VALUES (1, 1000);
 -- SELECT amount FROM orders ORDER BY amount DESC LIMIT 1;
 -- SELECT amount FROM orders ORDER BY amount ASC LIMIT 1;
+-- INSERT INTO orders(customer_id, amount) VALUES(2, 50);
 
 -- Funções de Agregação (Aggregate Functions)
 SELECT
-customer_id,
+  customer_id,
   COUNT(id) AS total_orders,
   SUM(amount) AS total_revenue,
   MAX(amount) AS max_order,
   MIN(amount) AS min_order,
   ROUND(AVG(amount), 2) AS average_ticket
 FROM orders
+-- WHERE total_orders > 1
 GROUP BY customer_id
-ORDER BY customer_id
+HAVING SUM(amount) BETWEEN 500 AND 1000
+ORDER BY average_ticket DESC
+-- LIMIT 3
 ;
