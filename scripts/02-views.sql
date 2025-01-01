b08b4958e71f666ec24569dce196eedc35b7d09e
Mateus Paiva <mateusopaiva@gmail.com>
2025-01-01 10:40:02 -0300

    O que são e como trabalhar com VIEWS

---
+++ scripts/02-views.sql
@@ -0,0 +1,22 @@
+\c live024
+
+--CREATE VIEW customers_summary AS 
+--  SELECT
+--    customer_id,
+--    COUNT(id) AS total_orders,
+--    SUM(amount) AS total_revenue,
+--    MAX(amount) AS max_order,
+--    MIN(amount) AS min_order,
+--    ROUND(AVG(amount), 2) AS average_ticket
+--  FROM orders
+--  GROUP BY customer_id
+--  ORDER BY customer_id
+--;
+
+--INSERT INTO orders(customer_id, amount) VALUES(4, 1000);
+
+SELECT * 
+FROM customers_summary
+--WHERE total_revenue BETWEEN 500 AND 1000
+ORDER BY total_revenue DESC
+;
