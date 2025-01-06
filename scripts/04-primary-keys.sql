f57f6a2e8179c71d30d7ee2b093e0a26e979326e
Mateus Paiva <mateusopaiva@gmail.com>
2025-01-06 16:55:59 -0300

    Entendendo a diferença entre constraints UNIQUE e Primary Keys

--- scripts/04-primary-keys.sql
+++ scripts/04-primary-keys.sql
@@ -1,10 +1,16 @@
 \c live024
 
-INSERT INTO customers(id, email) VALUES(1, 'qualquer_coisa');
+--INSERT INTO customers(id, email) VALUES(1, 'qualquer_coisa');
 
 --ALTER TABLE customers ADD PRIMARY KEY(id);
 --SELECT * FROM customers WHERE id = 1;
 
-SELECT constraint_name, constraint_type, table_name
-FROM information_schema.table_constraints
-WHERE table_schema = 'public';
+--ALTER TABLE customers ADD UNIQUE(email);
+--ALTER TABLE customers ADD PRIMARY KEY(id);
+
+--INSERT INTO customers(email) VALUES(null);
+INSERT INTO customers(ID, email) VALUES(null, 'asdffdsd');
+
+--SELECT constraint_name, constraint_type, table_name
+--FROM information_schema.table_constraints
+--WHERE table_schema = 'public';
