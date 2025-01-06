a6a98078bf7d90565f80081fb43165cd20c35b01
Mateus Paiva <mateusopaiva@gmail.com>
2025-01-06 16:07:34 -0300

    Criando constraints de UNIQUE compostas

--- scripts/03-constraints.sql
+++ scripts/03-constraints.sql
@@ -8,8 +8,16 @@
 
 --ALTER TABLE customers ALTER COLUMN email SET NOT NULL;
 --ALTER TABLE customers ADD CONSTRAINT customers_email_unique UNIQUE(email);
-ALTER TABLE customers ADD UNIQUE(email);
---ALTER TABLE customers DROP CONSTRAINT customers_email_unique;
+--ALTER TABLE customers ADD UNIQUE(email, id);
+--ALTER TABLE customers DROP CONSTRAINT customers_email_id_key;
+
+DROP TABLE batatinha;
+CREATE TABLE batatinha(
+  email VARCHAR(256) NOT NULL,
+
+  CONSTRAINT batatinha_email_unique_do_mateus UNIQUE(email)
+);
+
 
 SELECT constraint_name, constraint_type, table_name
 FROM information_schema.table_constraints
