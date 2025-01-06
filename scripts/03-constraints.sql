5db366f9a7c3644119fecb499f01e5a97942cb2a
Mateus Paiva <mateusopaiva@gmail.com>
2025-01-06 16:27:27 -0300

    Criando validações customizadas com a constraint CHECK

--- scripts/03-constraints.sql
+++ scripts/03-constraints.sql
@@ -11,13 +11,20 @@
 --ALTER TABLE customers ADD UNIQUE(email, id);
 --ALTER TABLE customers DROP CONSTRAINT customers_email_id_key;
 
-DROP TABLE batatinha;
-CREATE TABLE batatinha(
-  email VARCHAR(256) NOT NULL,
+--DROP TABLE batatinha;
+--CREATE TABLE batatinha(
+--  email VARCHAR(256) NOT NULL,
+--  CONSTRAINT batatinha_email_unique_do_mateus UNIQUE(email)
+--);
 
-  CONSTRAINT batatinha_email_unique_do_mateus UNIQUE(email)
-);
+--ALTER TABLE orders ADD CHECK(amount >= 0);
 
+--INSERT INTO orders(customer_id, amount) VALUES(10, 10);
+
+--ALTER TABLE orders DROP CONSTRAINT orders_amount_check1;
+
+--UPDATE orders SET amount = 917.71 WHERE id = 1;
+--SELECT * FROM orders WHERE id = 1;
 
 SELECT constraint_name, constraint_type, table_name
 FROM information_schema.table_constraints
