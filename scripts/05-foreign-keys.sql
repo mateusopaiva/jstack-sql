c7fd63a7e2b4303aa1855afdd6fe9597610738b8
Mateus Paiva <mateusopaiva@gmail.com>
2025-01-08 10:22:15 -0300

    Integridade Referencial: ON DELETE e ON UPDATE (CASCADE, RESTRICT, e mais)

--- scripts/05-foreign-keys.sql
+++ scripts/05-foreign-keys.sql
@@ -1,12 +1,21 @@
 \c live024
 
+-- Sobre a aula: RESTRICT/NO ACTION, CASCADE, SET NULL e SET DEFAULT
+
+-- ALTER TABLE orders ALTER COLUMN customer_id SET DEFAULT 20;
+
+-- ALTER TABLE orders DROP CONSTRAINT orders_fkey;
+-- ALTER TABLE orders 
+-- ADD CONSTRAINT orders_fkey FOREIGN KEY(customer_id) REFERENCES customers(id)
+-- ON UPDATE CASCADE
+-- ON DELETE SET DEFAULT
+-- ;
+
 --INSERT INTO orders(customer_id, amount) VALUES (12938, 1000);
 
---ALTER TABLE orders ADD FOREIGN KEY(customer_id) REFERENCES customers(id);
+--DELETE FROM orders WHERE customer_id = 1;
+--DELETE FROM customers WHERE id = 1;
 
---DROP TABLE IF EXISTS batatinha;
---CREATE TABLE batatinha(
---  customer_id INT REFERENCES customer(id)
---);
-
---ALTER TABLE batatinha drop CONSTRAINT chaveestrangeiradabatatinha
+--UPDATE customers SET id = 9999 WHERE id = 5;
+DELETE FROM customers WHERE id = 6;
+SELECT * FROM orders WHERE id = 6;
